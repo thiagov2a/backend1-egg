0cbab286956b4e62ddb6e6319129b2d1f7512518
Thiago Vildosa <tvildosa@gmail.com>
2023-03-27 21:38:08 -0300

    subida de ejercicios

--- Guia 5/Encuentro 9-11/POOEj1/src/pooej1/POOEj1.java
+++ Guia 5/Encuentro 9-11/POOEj1/src/pooej1/POOEj1.java
@@ -6,7 +6,6 @@
 package pooej1;
 
 import Entidad.Persona;
-import java.util.Scanner;
 
 /**
  *
@@ -18,14 +17,12 @@ public class POOEj1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        Scanner input = new Scanner(System.in); 
-        
+
         Persona p1 = new Persona("Rita", "Gayoso", "23/5/2000", "F", 4336897);
         Persona p2 = new Persona("Emilio", "Cerletti", "10/8/1980", "M", 3068453);
-        
+
         System.out.println(p1);
         System.out.println(p2);
     }
-    
+
 }
